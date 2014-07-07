515749a5609b759fc9e22d42254668d4eef44284
Samantha Woloszynski <sdw5fs@virginia.edu>
2014-07-07 02:47:12 +0200

    Add simple send text route based on twilio tutorial

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var twilio = require('twilio');
+var client = new twilio.RestClient();
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,6 +11,26 @@ router.get('/', function(req, res) {
 /* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
     res.render('helloworld', { title: 'Hello, World!' })
+});
+
+/* GET send text page. */
+router.get('/sendtext', function(req, res) {
+    client.sendSms({
+	    to:'+17036257313',
+	    from:'+17039917263',
+	    body:'HI FROM SAM'
+	}, function(error, message) {
+	    if (!error) {
+	        console.log('Success! The SID for this SMS message is:');
+	        console.log(message.sid);
+	 
+	        console.log('Message sent on:');
+	        console.log(message.dateCreated);
+	    } else {
+	        console.log('Oops! There was an error.');
+	    }
+	});
+    res.render('sendtext', { message: 'Hello!' })
 });
 
 module.exports = router;
